5ee0ba9cfa0209373b6c74c1ac7d69b450670770
paintedlbird7 <rosaperezlarios@gmail.com>
2021-05-14 17:49:34 -0700

    solved fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,30 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// make a for loop to console.log the numbers 1-5
+// if (n% 3) = fizz
+// if (n%5) = buzz
+// if (n%3 && n%5) = fizzbuzz
 
-module.exports = fizzBuzz;
+function fizzBuzz(n) {
+    //step 1: To console logs 1 to n write a for loop to loop through the numbers // starts at 1
+    for (let i = 1; i <= n; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+          console.log('fizzbuzz');
+      // step 2: // is the number a multiple of 3 and 5?
+      } else if (i % 3 === 0) {
+          // if true and there is no remainder print 'fizz'
+          console.log('fizz');
+        } else if (i % 5 === 0) {
+        // step 3:  if n%5 === 0,
+        console.log('buzz'); // if true and there is no remainder print 'buzz'
+      } else {
+          console.log(i); // iterates through i
+      } 
+    }
+  }
+  
+  fizzBuzz(5);
+  
+  module.exports = fizzBuzz;
+  
